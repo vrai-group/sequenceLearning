a5bcc04d2b907b6df2b123d7902031f2de8c110e
lorengin <lorenzok00@gmail.com>
2017-08-19 15:57:46 +0200

    [sequence-learning.py] early code for hiddem markov chain model

--- sequence-learning.py
+++ sequence-learning.py
@@ -3,6 +3,9 @@
 import numpy as np
 import datetime
 import re
+from sklearn.metrics import confusion_matrix
+from seqlearn.hmm import MultinomialHMM
+from seqlearn.evaluation import SequenceKFold
 
 filename = "./dataset/data"  # 15 Milan dataset   http://ailab.wsu.edu/casas/datasets/
 
@@ -55,7 +58,7 @@ sensorsList = sorted(set(sensors))
 dictSensors = {}
 for i, sensor in enumerate(sensorsList):
     dictSensors[sensor] = i
-print(dictSensors)
+#print(dictSensors)
 
 activityList = sorted(set(activities))
 dictActivities = {}
@@ -66,7 +69,7 @@ valueList = sorted(set(values))
 dictValues = {}
 for i, v in enumerate(valueList):
     dictValues[v] = i
-print(dictValues)
+#print(dictValues)
 
 dictObs = {}
 count = 0
@@ -85,7 +88,7 @@ for key in dictSensors.keys():
         for temp in range(0, int((max(temperature) - min(temperature)) * 2) + 1):
             dictObs[key + str(float(temp / 2.0) + min(temperature))] = count + temp
 
-print (dictObs)
+#print (dictObs)
 X = []
 Y = []
 for kk, s in enumerate(sensors):
@@ -94,3 +97,54 @@ for kk, s in enumerate(sensors):
     else:
         X.append(dictObs[s + str(values[kk])])
     Y.append(dictActivities[activities[kk]])
+
+def seq_lengths(labels):
+    lengths = []
+    for i, n in enumerate(labels):
+       if (i == 0):
+          seq = n
+          count = 1
+       else:
+           if (seq == n):
+               count += 1
+           else:
+               if (seq != n):
+                   seq = n
+                   lengths.append(count)
+                   count = 1
+       if (i == (len(Y)-1)):
+            lengths.append(count)
+    return lengths
+
+print('Creating folds ...')
+kf = SequenceKFold(seq_lengths(Y),20)
+for tuple in kf:
+    train_index = tuple[0]
+    train_len = tuple[1]
+    test_index = tuple[2]
+    test_len = tuple[3]
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
+    for idx in train_index:
+        X_train.append(X[idx])
+        Y_train.append(Y[idx])
+    for idx in test_index:
+        X_test.append(X[idx])
+        Y_test.append(Y[idx])
+    X_train = np.array(X_train).reshape(-1,1)
+    Y_train = np.array(Y_train)
+    X_test = np.array(X_test).reshape(-1,1)
+    Y_test = np.array(Y_test)
+    clf = MultinomialHMM()
+    clf.fit(X_train,Y_train,train_len)
+    Y_pred = clf.predict(X_test,test_len)
+    print('Train set:')
+    print(Y_train)
+    print('Test set:')
+    print(Y_test)
+    print('Prediction:')
+    print(Y_pred)
+    print('Confusion matrix:')
+    print(confusion_matrix(Y_test,Y_pred))
